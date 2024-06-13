cb928bbceb83894097823a775d5e636c39353208
Dmitry Kalinin <34896178+dsitd@users.noreply.github.com>
2024-06-13 11:34:21 +0300

    Update __init__.py

--- src/ml/models/__init__.py
+++ src/ml/models/__init__.py
@@ -1,1 +1,9 @@
-from .text import TextRequest
+from .llm_transformer import MYLLMGraphTransformer
+from .neo4j_vector import MyNeo4jVect
+from .colbert import colbert_reranker
+
+__all__ = [
+    "MYLLMGraphTransformer",
+    "MyNeo4jVect",
+    "colbert_reranker",
+]
