a9d9a01467a2752c4df9619438b92c6562932d8e
Dmitry Kalinin <34896178+dsitd@users.noreply.github.com>
2024-06-13 11:33:49 +0300

    Create colbert.py

---
+++ src/ml/models/colbert.py
@@ -0,0 +1,14 @@
+import dspy
+import numpy as np
+import tabulate
+from colbert.infra.config import ColBERTConfig
+
+# Конфигурация ColBERT
+colbert_config = ColBERTConfig()
+colbert_config.index_name = "Colbert-RM"
+colbert_config.experiment = "Colbert-Experiment"
+
+# Инициализация ColBERT reranker
+colbert_reranker = dspy.ColBERTv2RerankerLocal(
+    checkpoint="antoinelouis/colbert-xm", colbert_config=colbert_config
+)
